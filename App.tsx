547c55eda9d9fdff7739a2ff91f5ec69bb7d7888
Emre Can Akyol <emrecanakyol@Emre-MacBook-Pro.local>
2024-01-08 15:36:08 +0300

    Webview created.

--- App.tsx
+++ App.tsx
@@ -1,118 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useRef, useCallback, useEffect} from 'react';
 import {
   SafeAreaView,
+  RefreshControl,
   ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  BackHandler,
 } from 'react-native';
+import {WebView} from 'react-native-webview';
+import SplashScreen from 'react-native-splash-screen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [refresherEnabled, setEnableRefresher] = useState(true);
+  const webViewRef: any = useRef();
+  const [canGoBack, setCanGoBack] = useState(false);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const [apiData, setApiData] = useState('');
+  console.log("apiData", apiData)
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const FetchApiData = async () => {
+    await fetch('https://kuskayit.com/appApi.json')
+      .then(res => res.json())
+      .then(res => {
+        setApiData(res.kuskayit);
+      });
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+  const handleBack = useCallback(() => {
+    if (canGoBack && webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  }, [canGoBack]);
+
+  //Pull To Down Refresh
+  const handleScroll = (res: any) => {
+    const yOffset = Number(res.nativeEvent.contentOffset.y);
+    if (yOffset === 0) {
+      setEnableRefresher(true);
+    } else {
+      setEnableRefresher(false);
+    }
+  };
+
+  useEffect(() => {
+    FetchApiData();
+    SplashScreen.hide();
+    BackHandler.addEventListener('hardwareBackPress', handleBack);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBack);
+    };
+  }, [handleBack]);
+
+  return ( 
+    <SafeAreaView style={{flex: 1}}>
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        contentContainerStyle={{flex: 1}}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            enabled={refresherEnabled}
+            onRefresh={() => {
+              webViewRef.current.reload();
+            }}
+          />
+        }>
+        <WebView
+          source={{uri: 'https://www.kuskayit.com/admin/dist/login.php'}}
+          onLoadProgress={event => setCanGoBack(event.nativeEvent.canGoBack)}
+          ref={webViewRef}
+          originWhitelist={['*']}
+          onScroll={handleScroll}
+          allowsInlineMediaPlayback
+          javaScriptEnabled
+          scalesPageToFit
+          mediaPlaybackRequiresUserAction={false}
+          javaScriptEnabledAndroid
+          useWebkit
+          startInLoadingState
+          cacheEnabled
+          allowsFullscreenVideo
+          setBuiltInZoomControls
+        />
       </ScrollView>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
